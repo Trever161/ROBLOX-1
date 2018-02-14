8e5324786bda7e7d0dd8676853fd4d9e5519fdd8
Trever161 <trever1171@gmail.com>
2018-02-14 12:37:30 -0500

    Update Admin kolsh.lua

--- Admin kolsh.lua
+++ Admin kolsh.lua
@@ -1,3 +1,4 @@
+                  ----NOT MADE BY ME-----
 local owners = {"NikepRro2004"} -- Are able to set admins who can ban/etc... using :pa name
 local admins = {"NikePro2004"} -- Sets admins who can use ban/kick/admin or shutdown
 local tempadmins = {} -- Sets admins who can't use ban/kick/admin or shutdown
